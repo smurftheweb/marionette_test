d1c7deb8b0929878755f7c2ca471b138359ed2d3
smurftheweb <smurftheweb@gmail.com>
2016-11-11 21:32:15 +0000

    Change to collection

--- app/components/StartView.js
+++ app/components/StartView.js
@@ -2,10 +2,8 @@ import template from 'templates/layout';
 
 export default Marionette.View.extend({
   template: template,
-  model: new Backbone.Model({
-    items: [
+  collection: new Backbone.Collection([
       { assignee: "Scott", text: "Write a text book" },
       { assignee: "Rob", text: "Rewrite it using brunch" }
-    ]
-  })
+    ])
 });
